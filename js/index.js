e0db27a4e6dbb086b78baa1bd663a3e022e2af41
Vi <vikamatuchko772@gmail.com>
2023-04-08 17:58:43 +0300

    working Drop uploading simulation

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
-const form = document.querySelector(".drop__area"); //dropArea
-fileInput = form.querySelector(".drop__btn-hidden"); //dropBtnHidden
-(progressArea = document.querySelector(".drop__progress-area")),
-  (uploadedArea = document.querySelector(".drop__done-area")),
-  (dropText = document.querySelector(".drop__do-that"));
-//doneArea
+const form = document.querySelector(".drop__area");
+const fileInput = form.querySelector(".drop__btn-hidden");
+const progressArea = document.querySelector(".drop__progress-area");
+const uploadedArea = document.querySelector(".drop__done-area");
+const dropText = document.querySelector(".drop__do-that");
 
 let uploadedFile;
 
@@ -73,7 +72,9 @@ function uploadFile(name) {
 
   xhr.send(formData);
 }
-///////////////////////////////
+
+//Вирішила додати можливість перенести файли за допомогою
+//Drag&Drop, тому додала симуляцію завантаження файлу
 
 form.addEventListener("dragover", (e) => {
   e.preventDefault();
@@ -95,24 +96,8 @@ form.addEventListener("drop", (e) => {
   giveSize(file, fileName);
   dropText.innerHTML = "Browse or Drop Files to Upload";
   form.classList.remove("active");
-
-  // let file = e.target.files[0];
 });
 
-// function showFile() {
-//   let fileType = uploadedFile.type;
-
-//   let fileRead = new FileReader();
-
-//   fileRead.onload = () => {
-//     let fileUrl = fileRead.result;
-
-//     let imgTag = `<img src='${fileUrl}' alt='uploaded image' class='uploadedImg'>`;
-//     form.innerHTML = imgTag;
-//   };
-
-//   fileRead.readAsDataURL(uploadedFile);
-// }
 function giveSize(file, name) {
   let fileName = name;
   if (fileName.length >= 12) {
@@ -126,73 +111,45 @@ function giveSize(file, name) {
 
   function uploadFileDrop(name, size) {
     let xhr = new XMLHttpRequest();
-
     xhr.open("POST", "php/upload.php");
-
     xhr.upload.addEventListener("progress", ({ loaded, total }) => {
       total = size;
       let step = size * 0.2;
 
-      let result = 0;
-        function newLoaded() {
-          while (result <= size) {
-            for (let i = 0; i < 5; i++) {
-              result += step;
-            }
+      let count = 0;
+      loaded = 0;
 
-            return Math.round(result);
-            console.log(result);
-          }
-        }
+      function timer() {
+        setTimeout(function () {
+          loaded += step;
 
-    //   function timer() {
-    //     setTimeout(function () {
-    //       result += step;
-    //       console.log(result);
-    //       count++;
-    //       if (count <= 5) {
-    //         timer();
-    //       }
-    //     }, 1000);
-    //     Math.round(result);
-    //   }
+          let fileLoaded = Math.floor((loaded / total) * 100);
+          let fileTotal = Math.floor(total / 1000);
+          let fileSize;
 
-    //   timer();
+          fileTotal < 1024
+            ? (fileSize = fileTotal + " KB")
+            : (fileSize = (loaded / (1024 * 1024)).toFixed(2) + " MB");
 
-      loaded = newLoaded();
-      console.log(loaded, total);
+          let progressHTML = `<div class="drop__row">
+                  <i class="fa-solid fa-file-arrow-up drop__progress-icon"></i>
+                  <div class="drop__content">
+                    <div class="drop__details">
+                      <span class="drop__name">${name} · Uploading</span>
+                      <span class="drop__percent">${fileLoaded}%</span>
+                    </div>
+                    <div class="drop__progress-bar">
+                      <div class="progress" style='width: ${fileLoaded}%'></div>
+                    </div>
+                  </div>
+                </div>`;
 
-      let fileLoaded = Math.floor((newLoaded() / total) * 100);
-      let fileTotal = Math.floor(total / 1000);
-      let fileSize;
+          uploadedArea.classList.add("onprogress");
+          progressArea.innerHTML = progressHTML;
 
-      let count = 1;
-
-      let smt = 0;
-
-      fileTotal < 1024
-        ? (fileSize = fileTotal + " KB")
-        : (fileSize = (newLoaded() / (1024 * 1024)).toFixed(2) + " MB");
-
-      //   let progressHTML = `<div class="drop__row">
-      //         <i class="fa-solid fa-file-arrow-up drop__progress-icon"></i>
-      //         <div class="drop__content">
-      //           <div class="drop__details">
-      //             <span class="drop__name">${name} · Uploading</span>
-      //             <span class="drop__percent">${fileLoaded}%</span>
-      //           </div>
-      //           <div class="drop__progress-bar">
-      //             <div class="progress" style='width: ${fileLoaded}%'></div>
-      //           </div>
-      //         </div>
-      //       </div>`;
-
-      //   uploadedArea.classList.add("onprogress");
-      //   progressArea.innerHTML = progressHTML;
-
-      if (newLoaded() == total) {
-        progressArea.innerHTML = "";
-        let uploadedHTML = `<div class="drop__row">
+          if (loaded.toFixed() == total) {
+            progressArea.innerHTML = "";
+            let uploadedHTML = `<div class="drop__row">
                     <div class="drop__content">
                       <i class="fa-solid fa-file-arrow-up drop__progress-icon"></i>
                       <div class="drop__details">
@@ -202,13 +159,22 @@ function giveSize(file, name) {
                     </div>
                     <i class="fa-solid fa-check drop__checkmark"></i>
                   </div>`;
-        uploadedArea.classList.remove("onprogress");
-        uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
+            uploadedArea.classList.remove("onprogress");
+            uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
+          }
+
+          count++;
+          if (count < 5) {
+            timer();
+          }
+        }, 300);
+        Math.round(loaded);
+        return loaded;
       }
+      timer();
     });
 
     let formData = new FormData(form);
-
     xhr.send(formData);
   }
 }
